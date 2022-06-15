912dafd262e04f619c93930cc523332a6758d8d4
andreir15 <andrei.razvn15@gmail.com>
2022-06-15 18:58:37 +0200

    navbar changes

--- cliente/src/components/NavBar.jsx
+++ cliente/src/components/NavBar.jsx
@@ -132,7 +132,7 @@ const NavBar = () => {
           <Menu className="menu">
             <Item href="/#home">Home</Item>
             <Item href="/#sobre-nosotros">About&nbsp;us</Item>
-            <Item href="/contact">Contact us</Item>
+            <Item href="/contact">Contact&nbsp;us</Item>
             <Item href="/register">Register</Item>
           </Menu>
         </Cen>
